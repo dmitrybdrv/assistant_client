fe26f14b83ddf29ef842b8730a8322a14d02f395
dmitrybdrv <incubator-it@protonmail.com>
2024-04-25 18:07:24 -0700

    loader.module.scss code fix

--- src/components/ui/loader/loader.tsx
+++ src/components/ui/loader/loader.tsx
@@ -5,12 +5,14 @@ import s from './loader.module.scss'
 export const Loader = () => {
 
     return (
-        <div className={s.modalWrapper}>
-            <div className={s.modalWindow}>
-                <img className={s.circle} src={loader} alt="loader" />
+        <>
+            <div className={s.modalWrapper}>
+                <div className={s.modalWindow}>
+                    <img className={s.circle} src={loader} alt="loader" />
+                </div>
             </div>
             <div className={s.overlay}></div>
-        </div>
+        </>
 )
 
 }
